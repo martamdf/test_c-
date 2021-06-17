087a7d0bd6d06af22ffdd4e9a27b80b1ead8c641
Marta Maquedano <marta.maquedano@gmail.com>
2021-06-17 23:19:04 +0200

    test frase

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new Dictionary<char, int>();
+            char[] testchars = args[0].ToCharArray();
+            int length = testchars.Length;
+            foreach (char c in testchars)
+            {
+                if (!map.ContainsKey(c))
+                {
+                    map[c] = 0;
+                }
+                map[c] = map[c] + 1;
+            }
+            // Cabecera de la Tabla
+            Console.WriteLine("----------------------");
+            Console.WriteLine("| CHAR. | % REPETICIÓN");
+            Console.WriteLine("----------------------");
+
+            // Resultados
+            foreach (var caracter in map)
+            {
+                double result = ((double)caracter.Value / length) * 100;
+                Console.WriteLine("|   " + caracter.Key + "   |    " + Math.Round(result, 2) + "%");
+            }
+        }
+    }
+}
+
